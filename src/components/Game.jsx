c490ef11c6aa9fabb244854aa86c47c6caf04f24
Muhammad-Nouman-690 <nomi690@hotmail.com>
2021-04-29 14:15:15 +0500

    tic tac toe web app

---
+++ src/components/Game.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { calculateWinner } from "../helper";
+import Board from "./Board";
+
+const styles = {
+  width: "200px",
+  margin: "20px auto",
+};
+
+const Game = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setXisNext] = useState(true);
+  const winner = calculateWinner(board);
+
+  const handleClick = (i) => {
+    const boardCopy = [...board];
+    //if user click an occupied square or if game is won, return
+    if (winner || boardCopy[i]) return;
+    //put an x or an o in the clicked square
+    boardCopy[i] = xIsNext ? "x" : "o";
+    setBoard(boardCopy);
+    setXisNext(!xIsNext);
+  };
+
+  //const jumpTo = () => {};
+
+  const renderMoves = () => {
+    return (
+      <button onClick={() => setBoard(Array(9).fill(null))}>Start Game</button>
+    );
+  };
+
+  return (
+    <>
+      <Board squares={board} onClick={handleClick} />
+      <div style={styles}>
+        <p>
+          {winner
+            ? "winner: " + winner
+            : "Next Player: " + (xIsNext ? "x" : "o")}
+        </p>
+        {renderMoves()}
+      </div>
+    </>
+  );
+};
+
+export default Game;
